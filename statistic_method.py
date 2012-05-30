9f8b32a49dd8f6e28b40cab8e52d9f94c7b638eb
Anna Glazek <anna.glazek@gmail.com>
2012-05-30 11:49:05 +0200

    dwa przebiegi w statistic_method (co nie wpływa na wynik...)

--- statistic_method.py
+++ statistic_method.py
@@ -3,13 +3,21 @@ import rank_method
 
 class StatisticMethod(rank_method.RankMethod):
 
+    def _firstPass(self, text):
+        for sent in text.getSentences():
+            for word in sent.getTokens():
+                for base in self.getBases(word):
+                    self.words[base] = 0
+                                
+    def _getConsidered(self, word):
+        return set(self.relatedWords(word)).intersection(set(self.words.keys()))
+
     def countStats(self, text):
+        self._firstPass(text)
         for sent in text.getSentences():
             for word in sent.getTokens():
                 if self._use_stopwords and self._stopwords.isStopWord(word.encode('utf8')): continue
-                related = self.relatedWords(word)
-                for form in related:
-                    if form not in self.words: self.words[form] = 0
+                for form in self._getConsidered(word):
                     self.words[form] += 1
     
     def rankSentences(self, text):
@@ -21,9 +29,9 @@ class StatisticMethod(rank_method.RankMethod):
             rank[sent.getOrdinalNumber()] = 0
             for word in sent.getTokens():
                 if self._use_stopwords and self._stopwords.isStopWord(word.encode('utf8')): continue
-                related = self.relatedWords(word)
-                if len(related) > 0:
-                    rank[sent.getOrdinalNumber()] += max([score for (base, score) in self.words.items() if base in related])*1./len(sent.getTokens())
+                considered = self._getConsidered(word)
+                if len(considered) > 0:
+                    rank[sent.getOrdinalNumber()] += max([score for (base, score) in self.words.items() if base in considered])*1./len(sent.getTokens())
             denom += rank[sent.getOrdinalNumber()]
         for i in rank: rank[i] /= denom
         return rank
